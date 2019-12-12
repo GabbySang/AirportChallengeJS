3a500655a85afab838e89ff04201210d0accc548
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-12-12 10:29:49 +0000

    fixed NO EXPECTATIONS, included extra describe features in weather spec to account for random selection. Tests passing.

--- spec/weatherSpec.js
+++ spec/weatherSpec.js
@@ -8,12 +8,24 @@ describe('Weather',function(){
   });
 
 
-  it('will be stormy when conditions are bad', function(){
-    if (Math.random > 0.2){ expect(function(){weather.isStormy}).toBe(true) }
+  describe('Stormy weather', function(){
+    beforeEach(function(){
+      Math.random > 0.2;
 
-  });
+      it('will be stormy when conditions are bad', function(){
+        { expect(function(){weather.isStormy}).toBe(true) }
 
-  it('will not be stormy when conditions are good', function(){
-    if (Math.random < 0.2){ expect(function(){weather.isStormy}).toBe(false) }
-})
+      });
+
+
+      describe('Good weather', function(){
+        beforeEach(function(){
+          Math.random < 0.2;
+          it('will not be stormy when conditions are good', function(){
+            { expect(function(){weather.isStormy}).toBe(false) }
+          })
+        })
+      })
+    })
+  })
 });
